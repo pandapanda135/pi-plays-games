baf9dddab961b433b2b5c60e57834aa484dd8850
pandapanda135 <127447238+pandapanda135@users.noreply.github.com>
2024-09-30 15:14:33 +0100

    obs websocket added and working

---
+++ lua.lua
@@ -0,0 +1,19 @@
+obs = obslua
+
+function get_scene_item_ids(scene_name)
+    local scene_source = obs.obs_get_source_by_name("Scene 6")
+    local scene = obs.obs_scene_from_source(scene_source)
+    local items = obs.obs_scene_enum_items(scene)
+
+    for _, scene_item in ipairs(items) do
+        local source = obs.obs_sceneitem_get_source(scene_item)
+        local scene_item_id = obs.obs_sceneitem_get_id(scene_item)
+        local source_name = obs.obs_source_get_name(source)
+        print("Source Name: " .. source_name .. " | SceneItem ID: " .. scene_item_id)
+    end
+
+    obs.sceneitem_list_release(items)
+    obs.obs_source_release(scene_source)
+end
+
+get_scene_item_ids("Scene 6")
--- pi_main.py
+++ pi_main.py
@@ -3,9 +3,10 @@ import keyboard
 from mpmath import mp
 import time
 from key_codes import *
-from dotenv import load_dotenv
-from dotenv import set_key
+from dotenv import load_dotenv, set_key
 from pathlib import Path
+import obs_websocket as obs
+
 load_dotenv("up.env")
 mp.dps = 5000 # Set decimal places
 
@@ -38,22 +39,45 @@ while True:
 pi = str(mp.pi)
 split_pi = list(pi)
 
+#   Source Name: Colour Source | SceneItem ID: 5(dont use)
+#   Source Name: Display Capture 3 | SceneItem ID: 6 (dont use)
+#   Source Name: B_button_pressed.png | SceneItem ID: 10
+#   Source Name: A_button_pressed.png | SceneItem ID: 19
+#   Source Name: A_button_pressed | SceneItem ID: 11
+#   Source Name: B_button | SceneItem ID: 9
+#   Source Name: up_button_pressed.png | SceneItem ID: 20
+#   Source Name: up_button | SceneItem ID: 15
+#   Source Name: right_button_pressed.png | SceneItem ID: 21
+#   Source Name: right_button | SceneItem ID: 16
+#   Source Name: left_button_pressed.png | SceneItem ID: 22
+#   Source Name: left_button | SceneItem ID: 17
+#   Source Name: down_button_pressed.png | SceneItem ID: 23
+#   Source Name: down_button | SceneItem ID: 18
+#  Source Name: start_button_pressed.png | SceneItem ID: 30
+#  Source Name: start_button | SceneItem ID: 25
+#  Source Name: select_button_pressed.png | SceneItem ID: 31
+#  Source Name: select_button | SceneItem ID: 26
+
 while True:
-    time.sleep(0.25) # change time between input if this is set to 0.25 it will take approx 0.5 seconds assuming button_hold_time is set to 0.25
+    time.sleep(0.5) # change time between input if this is set to 0.25 it will take approx 0.5 seconds assuming button_hold_time is set to 0.25
     #use this if you want time between each input but cant increase key held time
     #switch to select key press
     match split_pi[up]:
         case '0':
             HoldAndReleaseKey(W, button_hold_time)
+            obs.Toggle_obs(15,20,button_hold_time)
             print("w")
         case '1':
             HoldAndReleaseKey(A, button_hold_time)
+            obs.Toggle_obs(17,22,button_hold_time)
             print("a")
         case '2':
             HoldAndReleaseKey(D, button_hold_time)
+            obs.Toggle_obs(16,21,button_hold_time)
             print("d")
         case '3':
             HoldAndReleaseKey(S, button_hold_time)
+            obs.Toggle_obs(18,23,button_hold_time)
             print("s")
         #not great for pokemon emerald can be enabled if playing other game
         # case '4':
@@ -61,12 +85,15 @@ while True:
         #     print("z")
         case '5'|'4':
             HoldAndReleaseKey(X, button_hold_time)
+            obs.Toggle_obs(25,30,button_hold_time)
             print("x")
         case '6':
             HoldAndReleaseKey(E, button_hold_time)
+            obs.Toggle_obs(11,19,button_hold_time)
             print("e")
         case '7':
             HoldAndReleaseKey(Q, button_hold_time)
+            obs.Toggle_obs(9,10,button_hold_time)
             print("q")
         case '8':
             HoldAndReleaseKey(F, button_hold_time)
@@ -87,6 +114,9 @@ while True:
         print("pi reset")
     else:
         pass
+    
+    #find a way to improve this while keeping it all in one line on terminal
+    print(up-5,"|",up-4,"|",up-3,"|",up-2,"|",up-1,"|",up,"|",up+1,"|",up+2,"|",up+3,"|",up+4,"|",up+5)
     
     if keyboard.is_pressed('e'):
         break
